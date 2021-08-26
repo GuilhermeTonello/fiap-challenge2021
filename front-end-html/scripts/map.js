ecadc1119ad5f293c616a377ced2b6bf8b504d48
Jan merkel <janpetermr@gmail.com>
2021-08-26 18:41:44 -0300

    Mapa feito e implementado

---
+++ front-end-html/scripts/map.js
@@ -0,0 +1,30 @@
+// Initialize and add the map
+function initMap() {
+  // The location of Uluru
+  const uluru = { lat: -25.344, lng: 131.036 };
+
+  const locations = [
+    [-25.254242, 130.979822],
+    [-25.296709, 130.995692],
+    [-25.344, 131.036],
+  ];
+  // The map, centered at Uluru
+
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 11,
+    center: {lat:-25.304161, lng:131.019537},
+  });
+  // The marker, positioned at Uluru
+  const marker = new google.maps.Marker({
+    position: uluru,
+    map: map,
+  });
+
+  var i = 0;
+  for (i; i < locations.length; i++) {
+    new google.maps.Marker({
+        position: new google.maps.LatLng(locations[i][0],locations[i][1]),
+        map: map,
+      });
+  }
+}
