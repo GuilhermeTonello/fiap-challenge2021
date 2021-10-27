3a24d7ae5b266da0f98ed983e2d96d74d16dd5a6
Jan merkel <janpetermr@gmail.com>
2021-10-27 10:48:11 -0300

    Home page upgraded

---
+++ front-end-html/scripts/progressBar.js
@@ -0,0 +1,35 @@
+$(".progress").each(function(){
+  
+    var $bar = $(this).find(".bar");
+    var $val = $(this).find("span");
+    var perc = parseInt( $val.text(), 10);
+  
+    $({p:0}).animate({p:perc}, {
+      duration: 3000,
+      easing: "swing",
+      step: function(p) {
+        $bar.css({
+          transform: "rotate("+ (45+(p*1.8)) +"deg)", // 100%=180° so: ° = % * 1.8
+          // 45 is to add the needed rotation to have the green borders at the bottom
+        });
+        $val.text(p|0);
+      }
+    });
+  });$(".progress").each(function(){
+  
+    var $bar = $(this).find(".bar");
+    var $val = $(this).find("span");
+    var perc = parseInt( $val.text(), 10);
+  
+    $({p:0}).animate({p:perc}, {
+      duration: 3000,
+      easing: "swing",
+      step: function(p) {
+        $bar.css({
+          transform: "rotate("+ (45+(p*1.8)) +"deg)", // 100%=180° so: ° = % * 1.8
+          // 45 is to add the needed rotation to have the green borders at the bottom
+        });
+        $val.text(p|0);
+      }
+    });
+  });
