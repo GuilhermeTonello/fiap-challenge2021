609bcc18e762a7387c4aed0a1c60621c35f6b262
Guilherme C. Tonello <guitonello@outlook.com.br>
2021-11-01 22:26:19 -0300

    Reajustando urls de requisição de api

--- front-end-html/scripts/api_obd2.js
+++ front-end-html/scripts/api_obd2.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = 'https://guardian-obd2-ford-fiap.herokuapp.com';
+const API_BASE_URL = 'http://150.230.88.140:3000';
 
 const API_ENDPOINT_OBD2 = `${API_BASE_URL}/obd2`;
 
@@ -42,6 +42,43 @@ function generateObd2InfoById(id) {
                 <td>${obd2.ALTITUDE}</td>
             </tr>
         `);
+    })
+    .catch(e => {
+        fetch(`https://guardian-obd2-ford-fiap.herokuapp.com/obd2/?ID=${id}`)
+        .then(res => res.json())
+        .then(obd2Data => {
+            let obd2 = obd2Data[0];
+            obd2InfoField.insertAdjacentHTML('beforeend', `
+                <tr>
+                <td>GPS Time</td>
+                <td>${obd2.GPS_TIME}</td>
+                </tr>
+                <tr>
+                    <td>Device Time</td>
+                    <td>${obd2.DEVICE_TIME}</td>
+                </tr>
+                <tr>
+                    <td>Longitude</td>
+                    <td>${obd2.LONGITUDE}</td>
+                </tr>
+                <tr>
+                    <td>Latitude</td>
+                    <td>${obd2.LATITUDE}</td>
+                </tr>
+                <tr>
+                    <td>GPS Speed</td>
+                    <td>${obd2.GPS_SPEED}</td>
+                </tr>
+                <tr>
+                    <td>Horizontal Dilution of Precision</td>
+                    <td>${obd2.HORIZONTAL_DILUTION}</td>
+                </tr>
+                <tr>
+                    <td>Altitude</td>
+                    <td>${obd2.ALTITUDE}</td>
+                </tr>
+            `);
+        })
     });
 }
 
