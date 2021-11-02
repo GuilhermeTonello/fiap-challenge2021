996912dfa4e26b63e2e99a5fff7f5c95402c45fa
Guilherme C. Tonello <guitonello@outlook.com.br>
2021-11-01 21:20:22 -0300

    Adicionando km rodados

--- front-end-html/scripts/api_infos_veiculos.js
+++ front-end-html/scripts/api_infos_veiculos.js
@@ -3,6 +3,7 @@ const API_BASE_URL = 'https://fiap-api.apps.pd01e.edc1.cf.ford.com';
 const API_ENDPOINT_VEHICLES = `${API_BASE_URL}/vehicles`;
 const API_ENDPOINT_PACKAGES = `${API_BASE_URL}/packages`;
 const API_ENDPOINT_MODELS = `${API_BASE_URL}/models`;
+const API_ENDPOINT_TELEMETRY = `${API_BASE_URL}/telemetry`;
 
 function getVehicleById(id) {
     return fetch(`${API_ENDPOINT_VEHICLES}/${id}`)
@@ -22,19 +23,28 @@ function getModelByModelId(id) {
         .then(data => data);
 }
 
+function getTelemetryByVehicleId(id) {
+    return fetch(`${API_ENDPOINT_TELEMETRY}/?vehicleId=${id}`)
+        .then(res => res.json())
+        .then(data => data);
+}
+
 function generateVehicleInfo(vehicleId) {
     let vehicleInfoField = document.querySelector('div#container div#seuCarro div.panel ul');
     getVehicleById(vehicleId)
         .then(vehicleData => getPackageByPackageId(vehicleData.packageId)
             .then(packageData => getModelByModelId(packageData.modelId)
-                .then(modelData => {
-                    vehicleInfoField.insertAdjacentHTML('beforeend', `
-                        <li>ID: ${vehicleData.id}</li>
-                        <li>Modelo: ${modelData.descriptions.nameplate}</li>
-                        <li>Ano: ${packageData.modelYear}</li>
-                        <li>Placa: ${vehicleData.plate}</li>
-                    `);
-                })
+                .then(modelData => getTelemetryByVehicleId(vehicleId)
+                    .then(telemetryData => {
+                        vehicleInfoField.insertAdjacentHTML('beforeend', `
+                            <li>ID: ${vehicleData.id}</li>
+                            <li>Modelo: ${modelData.descriptions.nameplate}</li>
+                            <li>KM rodados: ${telemetryData[0].odometer.value}</li>
+                            <li>Ano: ${packageData.modelYear}</li>
+                            <li>Placa: ${vehicleData.plate}</li>
+                        `);
+                    })
+                )
             )
         );
 }
