ef13ac351ad930a976e6ff8f21ba58b4c30b5850
Guilherme C. Tonello <guitonello@outlook.com.br>
2021-11-02 22:53:00 -0300

    Mudando API

--- front-end-html/scripts/api_manutencao_preditiva.js
+++ front-end-html/scripts/api_manutencao_preditiva.js
@@ -1,7 +1,7 @@
 const API_BASE_URL_FORD = 'https://fiap-api.apps.pd01e.edc1.cf.ford.com';
 const API_ENDPOINT_TELEMETRY_FORD = `${API_BASE_URL_FORD}/telemetry`;
 
-const API_BASE_URL_PREDIZER = 'https://4b81-150-230-88-140.ngrok.io';
+const API_BASE_URL_PREDIZER = 'http://equipe-guardian.com.br/ford/py';
 const API_ENDPOINT_PREDIZER = `${API_BASE_URL_PREDIZER}/predizer`;
 
 function getTelemetryByVehicleId(id) {
@@ -21,47 +21,25 @@ function generateManutencaoPreditiva(vehicleId) {
             .then(predicaoApi => {
                 let kmRodadosField = document.querySelector('div.tabela div.table-data p span#S');
                 kmRodadosField.innerHTML = telemetryData[0].odometer.value;
-                
-                let predicao = predicaoApi
-                    .replace("[", "")
-                    .replace("]", "")
-                    .replace(".]", "")
-                    .replace("[  ", "")
-                    .replace(/\s/g, '')
-                    .split(".");
 
-                let manutencoesRecomendadas = [];
-                for (let i = 0; i < predicao.length; i++) {
-                    if (predicao[i] == '1' && i != 5) {
-                        switch (i) {
-                            case 0:
-                                manutencoesRecomendadas.push('Trocar óleo do motor');
-                                break;
-                            case 1:
-                                manutencoesRecomendadas.push('Trocar filtro do óleo do motor');
-                                break;
-                            case 2:
-                                manutencoesRecomendadas.push('Trocar filtro de combustível');
-                                break;
-                            case 3:
-                                manutencoesRecomendadas.push('Trocar filtro de pólem');
-                                break;
-                            case 4:
-                                manutencoesRecomendadas.push('Trocar filtro de ar');
-                                break;
-                        }
-                    }
-                    if (i == 5) {
-                        manutencoesRecomendadas.push(`Valor previsto: R$ ${predicao[i]}`);
-                    }
-                }
                 let manutencaoTable = document.querySelector('div.man_recomendada div.tabela');
-                for (let man of manutencoesRecomendadas) {
-                    manutencaoTable.insertAdjacentHTML("beforeend", `
-                    <div class="table-data">
-                        <p>${man}</p>
-                    </div>
-                `);
+
+                predicaoArray = predicaoApi
+                    .replace('[', '')
+                    .replace(']', '')
+                    .replaceAll("'", '')
+                    .split(',');
+
+                console.log(predicaoArray);
+                for (let predicao of predicaoArray) {
+                    predicao = predicao.trim();
+                    if (predicao != 0) {
+                        manutencaoTable.insertAdjacentHTML("beforeend", `
+                            <div class="table-data">
+                                <p>${predicao}</p>
+                            </div>
+                        `);
+                    }
                 }
 
                 // Ordem do array
